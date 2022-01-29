a13fcd816ca0c871765f451120b98e7340045103
Houeibib Elmoustapha <31520397+elmoustaphahoueibib@users.noreply.github.com>
2022-01-29 20:59:23 +0100

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -15,7 +15,7 @@ const Hero = (props) => (
         <SectionText>
         Je suis un développeur Web et un concepteur d'interface utilisateur (Front End) basé à Nouakchott, Mauritanie. Mon travail est axé sur la création de produits fonctionnels, fiables et interactifs à partir de zéro. 
         </SectionText>
-        <Button onClick={props.handleClick}>En savoir</Button>
+        
       </LeftSection>
     </Section>
   </>
